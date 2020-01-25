eb57499b1e6408211a1a64cc3e42d3539e79b619
Akintade Ajibade <akintadeajibade@administorsmbp6.home>
2020-01-25 09:52:56 +0000

    added redux thunk

--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './rootReducer'
+import thunkMiddleware from 'redux-thunk'
 
 
 
-export const initializeStore = () => {
+export const initializeStore = (initialState={}) => {
   return createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware())
+    initialState,
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
   )
 }
