07ae104edfe0e6e627312d9023e750171f75db3b
Akintade Ajibade <ajibade.akintade@gmail.com>
2020-05-13 15:30:54 +0100

    added and configured  moneypensionservice/directories

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,16 @@ import { appWithTranslation } from "../components/translation/i18n";
 import withReduxStore from "../redux/with-redux-store";
 import { Provider } from "react-redux";
 
+import { ThemeProvider } from "@moneypensionservice/directories";
+
 class MyApp extends App {
   render() {
     const { Component, pageProps, reduxStore } = this.props;
     return (
       <Provider store={reduxStore}>
-        <Component {...pageProps} />
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Provider>
     );
   }
