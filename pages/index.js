e4937d404c3e3471fcee746cf78c1f2fe2640b6a
Akintade Ajibade <ajibade.akintade@gmail.com>
2020-05-14 11:37:29 +0100

    added the responsive structure for the landing pagge

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,47 @@ import PropTypes from "prop-types";
 
 import { withTranslation } from "../components/translation/i18n";
 
+import { Container, Row, Col } from "@moneypensionservice/directories";
+
 const Homepage = ({ t }) => {
   return (
     <Fragment>
-      <h1>Home</h1>
+      <Container debug>
+        <Row>
+          <Col sizes={{ xs: 12, md: 7 }} debug>
+            H1
+          </Col>
+          <Col sizes={{ xs: 12, md: 5 }} debug>
+            Anchor
+          </Col>
+        </Row>
+      </Container>
+      <Container debug>
+        <Row>
+          <Col sizes={{ xs: 12, md: 7 }} debug>
+            Required Question and link button
+          </Col>
+          <Col sizes={{ xs: 12, md: 5 }} debug>
+            Quote disclaimer
+          </Col>
+        </Row>
+      </Container>
+      <Container debug>
+        <Row>
+          <Col sizes={{ xs: 12, md: 6 }} debug>
+            Youtube Video
+          </Col>
+          <Col debug>FAQs</Col>
+        </Row>
+      </Container>
+      <Container debug>
+        <Row>
+          <Col sizes={{ xs: 12, md: 6 }} debug>
+            Articles &#x26; Guides
+          </Col>
+          <Col debug>Find a broker</Col>
+        </Row>
+      </Container>
     </Fragment>
   );
 };
