a94d9e0362add0b786f97257aa0353c95ee21f53
Akintade Ajibade <ajibade.akintade@gmail.com>
2020-05-14 14:44:24 +0100

    corrected import duplication

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,6 +1,5 @@
 import { Footer } from "@moneypensionservice/directories";
-import { i18n } from "../translation/i18n";
-import { withTranslation } from "../translation/i18n";
+import { i18n, withTranslation } from "../translation/i18n";
 
 const PageFooter = (props) => {
   return <Footer currentLgn={i18n.language} />;
--- components/header/index.js
+++ components/header/index.js
@@ -1,6 +1,5 @@
 import { Header } from "@moneypensionservice/directories";
-import { i18n } from "../translation/i18n";
-import { withTranslation } from "../translation/i18n";
+import { i18n, withTranslation } from "../translation/i18n";
 
 const PageHeader = ({ t, alternateAddress }) => {
   const changeLanguage = (e) => {
