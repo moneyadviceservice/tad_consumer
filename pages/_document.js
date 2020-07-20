85afec20a0bb9444d8482e50cfd7a9092544cb18
Akintade Ajibade <ajibade.akintade@gmail.com>
2020-07-20 08:43:49 +0100

    added Html component to TAD document structure

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,43 @@
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { i18n } from "../Utils/translation/i18n";
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
+    const lang = ctx.req.language + "-GB";
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
         });
       const initialProps = await Document.getInitialProps(ctx);
 
       return {
         ...initialProps,
+        locale: ctx.req.language + "-GB",
         styles: (
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        )
+        ),
       };
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang={this.props.locale}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
