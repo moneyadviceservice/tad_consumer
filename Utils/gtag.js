b09ad7eb2b1f87d0314fc23d44d0c73d9ab2bfc7
Akintade Ajibade <ajibade.akintade@gmail.com>
2020-10-02 11:57:33 +0100

    fixed tracking ID issue

--- Utils/gtag.js
+++ Utils/gtag.js
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = "GTM-WVFLH9>";
+export const GA_TRACKING_ID = "GTM-WVFLH9";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
