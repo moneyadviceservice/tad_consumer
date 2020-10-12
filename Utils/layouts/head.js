ae2ec6ddb7cf3aad65470f051e50c0267750bbef
andreferreiradlw <ferreirandre25@gmail.com>
2020-10-12 11:12:30 +0100

    Adds X-UA-Compatible meta tag to head

--- Utils/layouts/head.js
+++ Utils/layouts/head.js
@@ -21,6 +21,7 @@ const AppHead = ({ path, t }) => {
       <meta property="og:locale:alternate" content="en-GB" />
       <meta property="og:locale:alternate" content="cy-GB" />
       <meta property="og:description" content={t("head.description")} />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
       {/* <meta property="og:video" content="https://youtu.be/aijzHfO0VC8" /> */}
       <link
         rel="shortcut icon"
