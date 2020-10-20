b509b020ad691d2ab0cd1e8943add8805c5c8010
Akintade Ajibade <ajibade.akintade@gmail.com>
2020-10-20 12:55:41 +0100

    removed the change language function

--- components/header/index.js
+++ components/header/index.js
@@ -1,28 +1,16 @@
 import { Header } from "@moneypensionservice/directories";
 import { i18n, withTranslation } from "../../Utils/translation/i18n";
-import { useEffect } from "react";
 
 import Breadcrumb, { MobileBreadcrumb } from "../../Utils/layouts";
 
 const PageHeader = ({ t, alternateAddress, path }) => {
-  const changeLanguage = (e) => {
-    e.preventDefault();
-    i18n.changeLanguage(i18n.language === "en" ? "cy" : "en");
-  };
   let headerLang = !process.browser
     ? t("Header", { returnObjects: true })
     : t("Header", { returnObjects: true }).Header;
 
-  // let mobileBreadcrumb;
-
-  // useEffect(() => {
-  //   mobileBreadcrumb = path == "/listings" ? <MobileBreadcrumb t={t} /> : "";
-  // }, []);
-
   return (
     <Header
       currentLng={i18n.language}
-      setLng={(e) => changeLanguage(e)}
       lngUrl={alternateAddress}
       i18nLng={headerLang}
     >
