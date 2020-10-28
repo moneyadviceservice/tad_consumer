13aa50e80c547d13ac531fc8c7b61be9733884c7
Akintade Ajibade <ajibade.akintade@gmail.com>
2020-10-28 16:31:20 +0000

    rm breadcrumb

--- components/global.test.js
+++ components/global.test.js
@@ -27,71 +27,71 @@ describe("AppJs", () => {
 
 
 
-describe("Breadcrumb", () => {
+// describe("Breadcrumb", () => {
 
-    jest.mock("react-i18next", () => ({
-        withTranslation: () => (Component) => {
-          Component.defaultProps = { ...Component.defaultProps, t: () => [] };
-          return Component;
-        },
-      }));
+//     jest.mock("react-i18next", () => ({
+//         withTranslation: () => (Component) => {
+//           Component.defaultProps = { ...Component.defaultProps, t: () => [] };
+//           return Component;
+//         },
+//       }));
 
-      beforeEach(() => {
+//       beforeEach(() => {
        
-        nextRouter.useRouter = jest.fn();
-        nextRouter.useRouter.mockImplementation(() => ({ route: '/' }));
+//         nextRouter.useRouter = jest.fn();
+//         nextRouter.useRouter.mockImplementation(() => ({ route: '/' }));
         
-      })
+//       })
 
-    it("contains BreadAnchor", () => {
-        let props
-        props = {
-            path: "/"
-          };
+//     it("contains BreadAnchor", () => {
+//         let props
+//         props = {
+//             path: "/"
+//           };
         
-        const bread = shallow(<BreadCrumb {...props}/>).dive()
-        expect(bread.containsMatchingElement(<BreadAnchor/>))
+//         const bread = shallow(<BreadCrumb {...props}/>).dive()
+//         expect(bread.containsMatchingElement(<BreadAnchor/>))
         
-    })
-    it("contains MAS hompage link", () => {
-        let props
-        props = {
-            path: "/"
-          };
+//     })
+//     it("contains MAS hompage link", () => {
+//         let props
+//         props = {
+//             path: "/"
+//           };
         
-        const bread = shallow(<BreadCrumb {...props}/>).dive()
-        const MAS = bread.find(BreadAnchor)
-        expect(
-            MAS.findWhere((node) => node.prop("href") === "https://www.moneyadviceservice.org.uk/en")
-          ).toHaveLength(1);
+//         const bread = shallow(<BreadCrumb {...props}/>).dive()
+//         const MAS = bread.find(BreadAnchor)
+//         expect(
+//             MAS.findWhere((node) => node.prop("href") === "https://www.moneyadviceservice.org.uk/en")
+//           ).toHaveLength(1);
         
-    })
+//     })
 
-    it("contains BreadLink", () => {
-        let props
-        props = {
-            path: "/"
-          };
+//     it("contains BreadLink", () => {
+//         let props
+//         props = {
+//             path: "/"
+//           };
         
-        const bread = shallow(<BreadCrumb {...props}/>).dive()
-        expect(bread.containsMatchingElement(<BreadLink/>))
+//         const bread = shallow(<BreadCrumb {...props}/>).dive()
+//         expect(bread.containsMatchingElement(<BreadLink/>))
    
         
-    })
-    it("contain 2 BreadLinks if path is not '/' ", () => {
-        let props
-        props = {
-            path: "/listings"
-          };
+//     })
+//     it("contain 2 BreadLinks if path is not '/' ", () => {
+//         let props
+//         props = {
+//             path: "/listings"
+//           };
         
-        const bread = shallow(<BreadCrumb {...props}/>).dive()
-        const breadlink = bread.find(BreadLink)
-        expect(breadlink.length).toBe(2)
-        expect(breadlink.at(0).findWhere((node) => node.prop("href") === "/")).toHaveLength(1);
-        expect(breadlink.at(1).findWhere((node) => node.prop("href") === "listings")).toHaveLength(1);
+//         const bread = shallow(<BreadCrumb {...props}/>).dive()
+//         const breadlink = bread.find(BreadLink)
+//         expect(breadlink.length).toBe(2)
+//         expect(breadlink.at(0).findWhere((node) => node.prop("href") === "/")).toHaveLength(1);
+//         expect(breadlink.at(1).findWhere((node) => node.prop("href") === "listings")).toHaveLength(1);
       
 
         
-    })
+//     })
 
-})
+// })
