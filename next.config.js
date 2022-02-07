fcc9b59cf0d52902b64b6eb13792175d5796f3dd
Andrew Vos <andrew.vos@gmail.com>
2022-02-07 15:18:44 +0000

    Testing if this works in IE11

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 const withImages = require("next-images");
 module.exports = withImages();
 module.exports = {
-    webpack(config){
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ["@svgr/webpack"]
-        });
-        return config;
-    }
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+
+      if (
+        entries["main.js"] &&
+        !entries["main.js"].includes("./polyfills.js")
+      ) {
+        entries["main.js"].unshift("./polyfills.js");
+      }
+      return entries;
+    };
+
+    return config;
+  },
+};
