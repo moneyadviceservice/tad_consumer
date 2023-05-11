aaf15d548e132da10fe452d8f811f15d6a9be452
Lawrence Attoh <lawrence.attoh@pulselive.com>
2023-05-11 14:28:21 +0100

    Add text to travel insurance directory

--- server.js
+++ server.js
@@ -3,26 +3,24 @@ const next = require("next");
 const nextI18NextMiddleware = require("next-i18next/middleware").default;
 const bodyParser = require("body-parser");
 
-
 const nextI18next = require("./Utils/translation/i18n");
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handle = app.getRequestHandler();
 
 (async () => {
   await app.prepare();
   const server = express();
-  server.use(bodyParser.urlencoded({extended: true}));
-  server.use(bodyParser.json())
+  server.use(bodyParser.urlencoded({ extended: true }));
+  server.use(bodyParser.json());
 
   server.post("/listings", (req, res) => {
     let query = req.body;
-    console.log(query)
+    console.log(query);
 
-    if (query.age != ""){
-      let age = query.age
+    if (query.age != "") {
+      let age = query.age;
       query.cruise_30_days_max_age = age;
       query.cruise_45_days_max_age = age;
       query.cruise_50_days_max_age = age;
@@ -35,20 +33,18 @@ const handle = app.getRequestHandler();
     delete query.age;
     const queryString = () => {
       let out = [];
-      for (var key in query){
-        if(query.hasOwnProperty(key)){
+      for (var key in query) {
+        if (query.hasOwnProperty(key)) {
           out.push(key + "=" + encodeURIComponent(query[key]));
         }
       }
-      return out.join("&")
-    }
-    res.redirect("/listings?" + queryString())
-  })
+      return out.join("&");
+    };
+    res.redirect("/listings?" + queryString());
+  });
 
-
-
-  server.get("*",nextI18NextMiddleware(nextI18next) , (req, res) =>{
-    return handle(req, res)
+  server.get("*", nextI18NextMiddleware(nextI18next), (req, res) => {
+    return handle(req, res);
   });
 
   await server.listen(port);
