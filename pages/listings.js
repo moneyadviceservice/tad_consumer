6c0521b373d652e3129b50c40f1f464217a537b6
Ben Lovell <benjamin.lovell@gmail.com>
2024-12-03 10:29:37 +0000

    Add ACDL page load data layer events
    Co-authored-by: mariaTheod <maria.theodosopoulou@maps.org.uk>

--- pages/listings.js
+++ pages/listings.js
@@ -1,11 +1,15 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
-import pdfHelper from "../components/listingsPage/results/pdfHelper"
-import { withTranslation } from "../Utils/translation/i18n";
+import pdfHelper from "../components/listingsPage/results/pdfHelper";
+import { i18n, withTranslation } from "../Utils/translation/i18n";
 import styled from "styled-components";
 import { Col, resolveMedia } from "@moneypensionservice/directories";
 import { Section, ExtendedSection } from "../Utils/layouts";
-import { randomizeResult, listingsPDF, displayFirms} from "../components/listingsPage/utils";
+import {
+  randomizeResult,
+  listingsPDF,
+  displayFirms,
+} from "../components/listingsPage/utils";
 import Title from "../components/title";
 import {
   getAlgoFirms,
@@ -13,6 +17,8 @@ import {
 } from "../components/listingsPage/redux/actions";
 import Listing from "../components/listingsPage";
 import Breadcrumb from "../Utils/layouts";
+import { addEvent } from "../Utils/adobeAnalytics";
+import { useRouter } from "next/router";
 
 const ListingSection = styled(Section)`
   padding: 0;
@@ -22,19 +28,32 @@ const ListingSection = styled(Section)`
 `;
 
 const Listings = ({ t, query, offered }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    addEvent({
+      event: "pageLoadDirectory",
+      pageName: "tid-listings",
+      pageTitle: "Travel Adviser Directory | Travel firm listings",
+      lang: t("language"),
+      toolStep: "2",
+      stepName: "TID-Search results",
+    });
+  }, []);
+
   return (
     <Fragment>
       <Breadcrumb></Breadcrumb>
-      <ExtendedSection bgImg style={{marginBottom: "50px"}}>
+      <ExtendedSection bgImg style={{ marginBottom: "50px" }}>
         <Section constrained data-testid="contentRow">
-            <Col sizes={{ xs: 12 }} data-testid="contentCol">
+          <Col sizes={{ xs: 12 }} data-testid="contentCol">
             <Title />
-            </Col>
+          </Col>
         </Section>
       </ExtendedSection>
       <ExtendedSection align="stretch" style={{ paddingTop: 0 }}>
         <ListingSection constrained data-testid="contentRow">
-          <Listing t={t}  query={query} offered={offered}/>
+          <Listing t={t} query={query} offered={offered} />
         </ListingSection>
       </ExtendedSection>
     </Fragment>
@@ -48,37 +67,34 @@ Listings.getInitialProps = async ({ reduxStore, query, req, res }) => {
   randomizeResult(shuffledfirm);
   const offerings = await reduxStore.getState().listings.offerings.hits;
 
-  let filteredFirm = []
+  let filteredFirm = [];
 
   // pdf download query string
-  const exportPDF = query.listingsPDF === 'true';
+  const exportPDF = query.listingsPDF === "true";
   const isServer = !!req;
-  
+
   // create pdf and download when ?listingsPDF=true is added to listing page string
   if (isServer && exportPDF) {
     const buffer = await pdfHelper.componentToPDFBuffer(
-      listingsPDF(shuffledfirm)  
+      listingsPDF(shuffledfirm)
     );
-    res.setHeader(
-      'Content-disposition',
-      'attachment; filename="listings.pdf',
-      );
-    res.type('pdf');
+    res.setHeader("Content-disposition", 'attachment; filename="listings.pdf');
+    res.type("pdf");
     res.send(buffer);
   }
   // return shuffledfirm when nothing is submitted in non js environment
-  if(Object.keys(query).length === 0 && query.constructor === Object){
-    filteredFirm = shuffledfirm
+  if (Object.keys(query).length === 0 && query.constructor === Object) {
+    filteredFirm = shuffledfirm;
   }
   // return filtered firm when form is submitted to the backend
-  filteredFirm = displayFirms(query, offerings, shuffledfirm)
-  
+  filteredFirm = displayFirms(query, offerings, shuffledfirm);
+
   return {
     namespacesRequired: ["listings", "common", "footer"],
     query: query,
-    offered: filteredFirm
-   };
+    offered: filteredFirm,
   };
+};
 
 Listings.propTypes = {
   t: PropTypes.func.isRequired,
